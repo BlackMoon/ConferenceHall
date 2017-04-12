029dc0af147eb2eefb4197d2cb16f60b4cf39214
romanovrv <romanovrv@aquilon-st.ru>
2017-04-12 10:02:27 +0300

    Microsoft.AspNetCore.StaticFiles

--- Software/host/Startup.cs
+++ Software/host/Startup.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace host
 {
@@ -15,6 +19,7 @@ namespace host
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
             Configuration = builder.Build();
         }
 
@@ -33,6 +38,24 @@ namespace host
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
+            // exception handlers
+            app.UseExceptionHandler(builder =>
+            {
+                builder.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    IExceptionHandlerFeature ex = context.Features.Get<IExceptionHandlerFeature>();
+                    if (ex != null)
+                    {
+                        string error = JsonConvert.SerializeObject(new { ex.Error.Message });
+                        await context.Response.WriteAsync(error).ConfigureAwait(false);
+                    }
+                });
+            });
+
+            app.UseFileServer();
             app.UseMvc();
         }
     }
