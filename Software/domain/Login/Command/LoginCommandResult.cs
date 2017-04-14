26c70500acd1b25b4f2a24ea2186ff778cf17ab5
romanovrv <romanovrv@aquilon-st.ru>
2017-04-14 14:36:42 +0300

    _logger.LogError(msg);

---
+++ Software/domain/Login/Command/LoginCommandResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace domain.Login.Command
+{
+    public class LoginCommandResult
+    {
+        public LoginStatus Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Software/domain/Login/Command/LoginStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace domain.Login.Command
+{
+    public enum LoginStatus
+    {
+        /// <summary>
+        /// Sign in was successful
+        /// </summary>
+        Success = 0,
+        
+
+        /// <summary>
+        /// Sign in failed
+        /// </summary>
+        Failure = 3
+    }
+}
