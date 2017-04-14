23a94389b28163ac9e0ecb2520bae80fd2e1afe4
romanovrv <romanovrv@aquilon-st.ru>
2017-04-14 16:48:57 +0300

    Employee

---
+++ Software/domain/Employee/Employee.cs
@@ -0,0 +1,25 @@
+﻿using domain.Common;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace domain.Employee
+{
+    [Table("conf_hall.employees")]
+    public class Employee : KeyObject, IComponent
+    {
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("org_id")]
+        public int Org_Id { get; set; }
+
+        [Column("job_title")]
+        public bool JobTitle { get; set; }
+
+        [Column("phones_list")]
+        public string[] PhonesList { get; set; }
+
+        [Column("emails_list")]
+        public string[] EmailsList { get; set; }
+
+    }
+}
---
+++ Software/domain/Employee/Query/EmployeeQueryHandler.cs
@@ -0,0 +1,12 @@
+﻿using domain.Common.Query;
+using Kit.Dal.DbManager;
+
+namespace domain.Employee.Query
+{
+    public class EmployeeQueryHandler: KeyObjectQueryHandler<FindEmployeByIdQuery, Employee>
+    {
+        public EmployeeQueryHandler(IDbManager dbManager) : base(dbManager)
+        {
+        }
+    }
+}
