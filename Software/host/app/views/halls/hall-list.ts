ae57acd33162ae20872545cb378aa1555b98a536
romanovrv <romanovrv@aquilon-st.ru>
2017-04-18 18:34:22 +0300

    office_64x64.png

--- Software/host/app/views/halls/hall-list.ts
+++ Software/host/app/views/halls/hall-list.ts
@@ -1,6 +1,8 @@
-﻿import { Component, HostListener, Input, OnInit } from '@angular/core';
+﻿import { Component, ElementRef, Input, OnInit, Renderer } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HallService } from './hall.service';
+
+const selector = '.ui-panel';
 
 @Component({
     styleUrls: [`hall-list.css`],
@@ -10,12 +12,20 @@ export class HallListComponent implements OnInit {
 
     halls = [{ name: 'Создать', description: 'Новый халл' }];
 
-    constructor(private hallService: HallService) { }
+    constructor(
+        private elementRef: ElementRef,
+        private hallService: HallService,
+        private renderer: Renderer) {
+    }
 
     ngOnInit() {
 
         this.hallService
             .getAll()
             .subscribe(halls => this.halls = this.halls.concat(halls));
+    }
+
+    click() {
+        debugger;
     }
 }
