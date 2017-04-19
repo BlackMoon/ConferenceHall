10abd922e1b21f3c231dd4f516427c0deb2a8188
romanovrv <romanovrv@aquilon-st.ru>
2017-04-19 18:42:30 +0300

    /**  * Функция получения ошибки из http.Response  * @param error  */

---
+++ Software/host/app/common/http-error.ts
@@ -0,0 +1,24 @@
+﻿import { isDevMode } from '@angular/core';
+import { Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+
+/**
+ * Функция получения ошибки из http.Response
+ * @param error
+ */
+export function handleResponseError(error: Response | any): ErrorObservable {
+   
+    let errMsg: string;
+    if (error instanceof Response)
+    {
+        const body = error.json() || '';
+        const err = body.error || JSON.stringify(body);
+        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    }
+    else 
+        errMsg = error.message ? error.message : error.toString();
+    
+    isDevMode() && console.error(errMsg);
+    return Observable.throw(errMsg);
+}
