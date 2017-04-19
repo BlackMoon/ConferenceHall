71ca61d3f6882fd6e5af5ef77f419cb3403a75ff
istrashkinaeg <istrashkinaeg@aquilon-st.ru>
2017-04-19 15:38:31 +0300

    Справочник клиентов

---
+++ Software/host/app/views/employees/employees.module.ts
@@ -0,0 +1,17 @@
+﻿import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { DataGridModule, PanelModule } from 'primeng/primeng';
+
+import { EmployeeListComponent } from './employee-list';
+import { EmployeeDetailComponent } from './employee-detail';
+import { EmployeeRoutingModule } from './employees-routing.module';
+import { EmployeeService } from './employee.service';
+
+@NgModule({
+    declarations: [EmployeeDetailComponent, EmployeeListComponent],
+    exports: [DataGridModule],
+    imports: [CommonModule, DataGridModule, FormsModule, EmployeeRoutingModule, PanelModule],
+    providers: [EmployeeService]
+})
+export class EmployeesModule { }
