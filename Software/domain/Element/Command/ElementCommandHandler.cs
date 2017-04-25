0f2c03a55a77c5e054839392499b22c28fa8d11c
istrashkinaeg <istrashkinaeg@aquilon-st.ru>
2017-04-25 17:00:29 +0300

    ResizeImageBySkiaSharp

--- Software/domain/Element/Command/ElementCommandHandler.cs
+++ Software/domain/Element/Command/ElementCommandHandler.cs
@@ -3,6 +3,9 @@ using domain.Common.Command;
 using Dapper.Contrib.Extensions;
 using Kit.Core.CQRS.Command;
 using Kit.Dal.DbManager;
+using SkiaSharp;
+using System.IO;
+
 
 namespace domain.Element.Command
 {
@@ -18,14 +21,43 @@ namespace domain.Element.Command
             return DbManager.DbConnection.Insert(command);
         }
 
+        private byte[] ResizeImageBySkiaSharp(byte[] data, int size)
+        {
+            using (var ms = new MemoryStream(data))
+            {
+                var inputStream = new SKManagedStream(ms);
+                using (var original = SKBitmap.Decode(inputStream))
+                {
+                    int width, height;
+                    if (original.Width > original.Height)
+                    {
+                        width = size;
+                        height = original.Height * size / original.Width;
+                    }
+                    else
+                    {
+                        width = original.Width * size / original.Height;
+                        height = size;
+                    }
+
+                    using (var resized = original
+                           .Resize(new SKImageInfo(width, height), SKBitmapResizeMethod.Lanczos3))
+                    {
+                        if (resized == null) return data;
+                        return resized.Bytes;
+                    }
+
+                }
+            }
+        }
         public async Task<long> ExecuteAsync(Element command)
         {
-            command.Thumbnail = f(data)
+            
+
+           // command.Thumbnail = ResizeImageBySkiaSharp(command.Data,  command.Width)
 
 
-            await DbManager.OpenAsync();
-
-
+           // await DbManager.OpenAsync();
 
             return await DbManager.DbConnection.InsertAsync(command);
         }
