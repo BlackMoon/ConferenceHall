ca3468643df7e421e8e6aeba865d3a1a82dbe943
Roman <admin@mail.ru>
2017-05-13 15:05:12 +0300

    remove rimraf npm

--- Software/host/config/webpack.common.js
+++ Software/host/config/webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
 
     plugins: [     
 
-        new ContextReplacementPlugin(
+     new ContextReplacementPlugin(
             /angular(\\|\/)core(\\|\/)@angular/,
             helpers.root('./app')
         ),
