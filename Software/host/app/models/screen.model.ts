69cdf1aef3117e03ea4a6c29e903bb9e6feae13e
romanovrv <romanovrv@aquilon-st.ru>
2017-05-30 11:32:19 +0300

    screen.model

---
+++ Software/host/app/models/screen.model.ts
@@ -0,0 +1,24 @@
+﻿import { TimeRange } from './index';
+
+/**
+ * Модель. Активная конференция (на отдельном экране)
+ */
+export class ScreenModel {
+    height: number;
+    width: number;
+
+    subject: string;
+    plan: string;
+    period: TimeRange;
+
+    messages: string[];
+
+    constructor() {
+
+        this.subject = null;
+        this.plan = null;
+        this.period = null;
+        this.height = 2;
+        this.width = 2;
+    }
+}
