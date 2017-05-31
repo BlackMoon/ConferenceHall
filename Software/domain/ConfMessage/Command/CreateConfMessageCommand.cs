0bf4eda663f280cb59f3a7362068075e0da09127
Marat Gumerov <gumerovm@aquilon.ru>
2017-05-31 13:14:34 +0300

    добавил domain/ConfMessage

---
+++ Software/domain/ConfMessage/Command/CreateConfMessageCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Kit.Core.CQRS.Command;
+using System;
+
+namespace domain.ConfMessage.Command
+{
+    /// <summary>
+    /// Команда. Создание сообщения конференции
+    /// </summary>
+    public class CreateConfMessageCommand : ICommand
+    {
+        [Column("hall_id")]
+        public int ConfId { get; set; }
+
+        public string Message { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
---
+++ Software/domain/ConfMessage/Query/ConfMessageQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using domain.Common.Query;
+using Kit.Dal.DbManager;
+using Dapper;
+using Kit.Core.CQRS.Query;
+
+namespace domain.ConfMessage.Query
+{
+    public class ConfMessageQueryHandler : 
+        KeyObjectQueryHandler<FindConfMessageByIdQuery, ConfMessage>,
+        IQueryHandler<FindConfMessageQuery, IEnumerable<ConfMessage>>
+    {
+        public ConfMessageQueryHandler(IDbManager dbManager) : base(dbManager)
+        {
+        }
+
+        public IEnumerable<ConfMessage> Execute(FindConfMessageQuery query)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ConfMessage>> ExecuteAsync(FindConfMessageQuery query)
+        {
+
+            SqlBuilder sqlBuilder = new SqlBuilder("conf_hall.conf_messages s")
+                .Column("s.id")
+                .Column("s.conf_id")
+                .Column("s.message")
+                .Column("s.active")
+                 //  .Where("s.id = @id")
+                 .OrderBy("s.id");
+
+            DynamicParameters param = new DynamicParameters();
+
+            await DbManager.OpenAsync();
+            return await DbManager.DbConnection.QueryAsync<ConfMessage>(sqlBuilder.ToString(), param);
+
+        }
+    }
+}
