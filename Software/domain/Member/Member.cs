e601c7529cc6ebe2a55696666dd0c87a4a7496d8
istrashkinaeg <istrashkinaeg@aquilon-st.ru>
2017-05-31 08:08:33 +0300

    MembersQuery, Filter

--- Software/domain/Member/Member.cs
+++ Software/domain/Member/Member.cs
@@ -6,30 +6,20 @@ namespace domain.Member
 {
     [Table("conf_hall.employees")]
     public class Member : KeyObject
-    {
-        [Column("employee_id")]
-        [JsonIgnore]
-        public int EmployeeId { get; set; }
-
-        [Column("name")]
+    {     
         public string Name { get; set; }
                 
         [Column("job_title")]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string JobTitle { get; set; }
 
         [Column("emails_list")]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string[] EmailsList { get; set; }
 
         [Column("phones_list")]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string[] PhonesList { get; set; }
 
         public string Place { get; set; }
 
-        [Column("role")]
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Role { get; set; }
 
         [Column("locked")]
