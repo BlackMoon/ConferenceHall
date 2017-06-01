085b3a991b39986583d4e62804dfdbec1deb94e2
Marat Gumerov <gumerovm@aquilon.ru>
2017-06-01 13:37:58 +0300

    добавил view для confMessage-list

---
+++ Software/host/app/views/confMessages/confMessage-list.component.ts
@@ -0,0 +1,53 @@
+﻿import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ConfirmationService } from 'primeng/primeng';
+import { Logger } from "../../common/logger";
+import { ConfMessageModel } from '../../models/index';
+import { ConfMessageService } from './confMessage.service';
+
+@Component({
+    selector: 'confMessage-list',
+    templateUrl: 'confMessage-list.component.html'
+        
+})
+export class ConfMessageListComponent {
+
+    confMessageformVisible: boolean;
+    confMessageform: FormGroup;
+
+    @Input()
+    confId: number;
+
+    @Input()
+    items: ConfMessageModel[];
+
+    constructor(
+        private confirmationService: ConfirmationService,
+        private fb: FormBuilder,
+        private logger: Logger,
+        private router: Router,
+        private confMessageService: ConfMessageService) { }
+
+    ngOnInit() {
+
+        this.confMessageform = this.fb.group({
+            name: [null, Validators.required]
+        });
+    }
+
+    addConfMessage(confMessage) {
+       
+        confMessage.hallId = this.confId;
+        this.confMessageService
+            .add(confMessage)
+            .subscribe(
+                key => {
+                    confMessage.id = key;
+                    this.items.push(confMessage);
+                    this.confMessageformVisible = false;
+                    this.confMessageform.reset();
+                },
+                error => this.logger.error(error));
+    }
+}
---
+++ Software/host/app/views/confMessages/confMessages.module.ts
@@ -0,0 +1,53 @@
+﻿import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import {
+    AccordionModule,
+    ButtonModule,
+    DataGridModule,
+    DataListModule,
+    DragDropModule,
+    DropdownModule,
+    FieldsetModule,
+    InputTextModule,
+    InputTextareaModule,
+    MenuModule,
+    PanelModule,
+    SpinnerModule,
+    TabViewModule,
+    ToggleButtonModule,
+    ToolbarModule
+} from 'primeng/primeng';
+
+import { ElementsModule } from "../elements/elements.module";
+import { GroupsModule } from "../groups/groups.module";
+import { ConfMessageListComponent } from "./confMessage-list.component";
+import { ConfMessageService } from './confMessage.service';
+
+@NgModule({
+    declarations: [ConfMessageListComponent],
+    exports: [ButtonModule, DataGridModule, FieldsetModule, InputTextModule, InputTextareaModule, ConfMessageListComponent, SpinnerModule],
+    imports: [
+        AccordionModule,
+        ButtonModule,
+        CommonModule,
+        DataGridModule,
+        DataListModule,
+        DragDropModule,
+        DropdownModule,
+        ElementsModule,
+        FieldsetModule,
+        FormsModule,
+        GroupsModule,
+        InputTextModule,
+        InputTextareaModule,
+        MenuModule,
+        PanelModule,
+        ReactiveFormsModule,
+        SpinnerModule,
+        TabViewModule,
+        ToggleButtonModule,
+        ToolbarModule],
+    providers: [ConfMessageService]
+})
+export class ConfMessagesModule { }
