d0b329c3c7ce192f00b9b61858ffd471b9df07e9
Roman <admin@mail.ru>
2017-06-03 22:55:48 +0300

    Layout.ShowHeader

--- Software/host/app/app.component.ts
+++ Software/host/app/app.component.ts
@@ -1,7 +1,8 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Http, RequestOptions, URLSearchParams } from '@angular/http';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Message } from 'primeng/primeng';
-import { Router } from '@angular/router';
+import { Layout } from "./common/navigation/layout";
 import { Logger } from "./common/logger";
 
 const startViewKey = 'returnUrl';
@@ -13,26 +14,44 @@ const startViewKey = 'returnUrl';
 })
 export class AppComponent implements OnInit {
 
-    msgs: Message[] = [];
+    msgs: Message[] = [];    
+
+    private layout: Layout = Layout.None;
+    private Layout = Layout;
     /**
      * Стартовая страница
      */
     private startView: string;    
 
     constructor(
+        private route: ActivatedRoute,
         private logger: Logger,
         private router: Router)
     {
-        this.startView = new URLSearchParams(window.location.search.slice(1)).get(startViewKey);
-        
+        this.router
+            .events
+            .filter(e => e instanceof NavigationEnd)
+            .map(() => this.route)
+            .map(r => {
+                // find the last activated route
+                while (r.firstChild) r = r.firstChild;
+                return r;                
+            })
+            .filter(route => route.outlet === 'primary')
+            .mergeMap(r => r.data)
+            .subscribe(data => this.layout = data.layout | Layout.ShowHeader);
+
+        this.startView = new URLSearchParams(window.location.search.slice(1)).get(startViewKey);        
     }
 
     ngOnInit() {
-
+        
         this.logger.msgReсeived.subscribe((msgs: Message[]) => this.msgs = msgs);
 
         // start page --> navigate
         if (this.startView != undefined)
             this.router.navigateByUrl(this.startView);
     }
+
+    bitTest = (layout: Layout) => this.layout & layout;
 }
---
+++ Software/host/app/common/navigation/layout.ts
@@ -0,0 +1,16 @@
+﻿/**
+ * Макет
+ */
+
+export enum Layout
+{
+    None = 0,
+    
+    ShowHeader = 1 << 0,
+    
+    ShowFooter = 1 << 1,
+
+    ShowLeftSide = 1 << 2,
+
+    ShowRightSide = 1 << 3
+}
