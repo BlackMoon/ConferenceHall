bd5ed7bd1cc371407c4c2fff6e3246f44adbcff8
Roman <admin@mail.ru>
2017-06-03 22:56:18 +0300

    route.data

--- Software/host/app/common/navigation/navigation.module.ts
+++ Software/host/app/common/navigation/navigation.module.ts
@@ -1,12 +1,13 @@
 ﻿import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Layout } from './layout';
 import { SelectivePreloadingStrategy } from './selective-preloading-strategy';
 import { ViewsModule } from '../../views/views.module';
 import * as views from '../../views';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },    
-    { path: 'home', component: views.HomeView }
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: views.HomeView, data: { layout: Layout.ShowHeader | Layout.ShowFooter } }
 ];
 
 @NgModule({
