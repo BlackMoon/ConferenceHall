53e844f4cf3378426dab2575def912adb8b417c7
Roman <admin@mail.ru>
2017-06-04 12:32:05 +0300

    PartialUpdate --> JsonPatch

--- Software/host/Controllers/ConferencesController.cs
+++ Software/host/Controllers/ConferencesController.cs
@@ -10,6 +10,9 @@ using host.Hubs;
 using Kit.Core.CQRS.Command;
 using Microsoft.AspNetCore.SignalR.Infrastructure;
 using TimeRange = domain.Common.Range<System.DateTime>;
+using Microsoft.AspNetCore.JsonPatch;
+using System.Linq;
+using Microsoft.AspNetCore.JsonPatch.Operations;
 
 namespace host.Controllers
 {
@@ -40,20 +43,19 @@ namespace host.Controllers
         {
             value.ConferenceId = id;
             return CommandDispatcher.DispatchAsync<MakeAppointmentCommand, TimeRange>(value);
-        }
+        }        
 
-        [HttpPut("/api/period/{id}")]
-        public Task ChangePeriod(int id, [FromBody]ChangePeriodCommand value)
+        [HttpPatch("{id}")]
+        public Task Patch(int id, [FromBody]JsonPatchDocument patch)
         {
-            value.ConferenceId = id;
-            return CommandDispatcher.DispatchAsync<ChangePeriodCommand, bool>(value);
-        }
+            PartialUpdateCommand value = new PartialUpdateCommand()
+            {
+                ConferenceId = id                
+            };
 
-        [HttpPut("/api/state/{id}")]
-        public Task ChangeState(int id, [FromBody]ChangeStateCommand value)
-        {
-            value.ConferenceId = id;
-            return CommandDispatcher.DispatchAsync<ChangeStateCommand, bool>(value);
+            patch.ApplyTo(value);
+            
+            return CommandDispatcher.DispatchAsync<PartialUpdateCommand, bool>(value);
         }
 
         // POST api/conferences
