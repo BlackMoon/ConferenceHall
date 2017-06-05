582d5eef0eaa580c0338b105aabc00454af6f08a
romanovrv <romanovrv@aquilon-st.ru>
2017-06-05 08:41:43 +0300

    hall-list

---
+++ Software/host/app/views/halls/hall-grid.component.ts
@@ -0,0 +1,50 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfirmationService } from 'primeng/primeng';
+import { Logger } from "../../common/logger";
+import { HallModel } from '../../models';
+import { HallService } from './hall.service';
+
+@Component({
+    styleUrls: [`hall-grid.component.css`],
+    templateUrl: 'hall-grid.component.html'
+})
+export class HallGridComponent implements OnInit {
+
+    halls: HallModel[] = [<any>{ name: 'Создать', description: 'Новый холл' }];
+
+    constructor(
+        private confirmationService: ConfirmationService,
+        private hallService: HallService,
+        private logger: Logger) { }
+
+    ngOnInit() {
+
+        this.hallService
+            .getAll()
+            .subscribe(
+                halls => this.halls = this.halls.concat(halls),
+                error => this.logger.error(error));
+    }
+
+    removeHall(id:number, name?: string) {
+       
+        this.confirmationService.confirm({
+            header: 'Вопрос',
+            icon: 'fa fa-trash',
+            message: `Удалить [${name}]?`,
+            accept: () => 
+                
+                this.hallService
+                    .delete(id)
+                    .subscribe(
+                        _ => {
+                    
+                            let ix = this.halls.findIndex(h => h.id === id);
+                            this.halls.splice(ix, 1);
+                        },
+                        error => this.logger.error(error))
+            
+        });
+    }
+}
---
+++ Software/host/app/views/halls/hall-list.component.ts
@@ -0,0 +1,43 @@
+﻿import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Logger } from "../../common/logger";
+import { HallModel } from '../../models';
+import { HallService } from './hall.service';
+
+@Component({
+    selector: 'hall-list',
+    templateUrl: 'hall-list.component.html'
+})
+export class HallListComponent implements OnInit {
+
+    halls: HallModel[];
+    selectedHallIds: number[] = [];
+
+    @Output() selectionChanged = new EventEmitter<number[]>();
+
+    constructor(
+        private hallService: HallService,
+        private logger: Logger) {}
+
+    ngOnInit() {
+
+        this.hallService
+            .getAll()
+            .subscribe(
+                halls => this.halls = halls,
+                error => this.logger.error(error));
+    }
+
+    selectHall(hall) {
+        hall.selected = !hall.selected;
+
+        if (hall.selected)
+            this.selectedHallIds.push(hall.id);
+        else {
+            let ix = this.selectedHallIds.indexOf(hall.id);
+            this.selectedHallIds.splice(ix, 1);
+        }
+
+        this.selectionChanged.emit(this.selectedHallIds);
+    }
+}
