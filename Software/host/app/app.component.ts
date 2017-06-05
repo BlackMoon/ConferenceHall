26e92b54bc8851c11c7f22dfeb46be4fde3e32b1
Stanislav Fiseyskiy <fiseyskiysv@aquilon-st.ru>
2017-06-05 10:42:53 +0300

    Подправление subscriber router для варианта страницы Layout=None

--- Software/host/app/app.component.ts
+++ Software/host/app/app.component.ts
@@ -38,7 +38,7 @@ export class AppComponent implements OnInit {
                 return r;                
             })
             .mergeMap(r => r.data)
-            .subscribe(data => this.layout = data.layout | Layout.ShowHeader);
+            .subscribe(data => this.layout = (data.layout == Layout.None) ? (data.layout) : (data.layout || Layout.ShowHeader));
 
         this.startView = new URLSearchParams(window.location.search.slice(1)).get(startViewKey);        
     }
