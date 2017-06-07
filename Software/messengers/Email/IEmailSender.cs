0a3efd1eb6bf45bb87c7eee9e74e87c5b7e2f99d
Stanislav Fiseyskiy <fiseyskiysv@aquilon-st.ru>
2017-06-07 10:56:17 +0300

    нач изменения в проект messenger

---
+++ Software/messengers/Email/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿
+namespace host.EmailSystem
+{
+
+    public interface IEmailSender
+    {
+        void Send(string subject, string body, string[] email);
+    }
+
+
+}
---
+++ Software/messengers/MessageManagers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace messengers
+{
+    // 
+    public class MessageManagers
+    {
+        public string TypeMessager;
+        public string Message;
+    }
+}
