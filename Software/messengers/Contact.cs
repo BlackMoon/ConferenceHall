ce54452165a0622f360c11925a7cdcc080d986e4
romanovrv <romanovrv@aquilon-st.ru>
2017-06-07 16:40:06 +0300

    Contact

---
+++ Software/messengers/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace messengers
+{
+    public class Contact
+    {
+        /// <summary>
+        /// Вид контакта
+        /// </summary>
+        public string Kind { get; set; }
+
+        public string Address { get; set; }
+    }
+}
