e0e94101580ef68f3f1cf343b46f8b5aef5a9df7
romanovrv <romanovrv@aquilon-st.ru>
2017-06-07 10:54:27 +0300

    Contact

---
+++ Software/domain/Contact/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using domain.Common;
+
+namespace domain.Contact
+{
+    /// <summary>
+    /// Контакт
+    /// </summary>
+    [Table("conf_hall.contacts")]
+    public class Contact : KeyObject
+    {
+        /// <summary>
+        /// Вид контакта
+        /// </summary>
+        public string Kind { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
