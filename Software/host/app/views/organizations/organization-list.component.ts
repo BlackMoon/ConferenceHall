48c008955b33565758350c0fb252de0f8aa27dfc
romanovrv <romanovrv@aquilon-st.ru>
2017-06-08 14:45:58 +0300

    organization.service.ts

---
+++ Software/host/app/views/organizations/organization-list.component.ts
@@ -0,0 +1,12 @@
+﻿import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Logger } from "../../common/logger";
+
+@Component({
+    selector: "organization-list",
+    templateUrl: 'organization-list.component.html'
+})
+
+export class OrganizationListComponent {
+}
