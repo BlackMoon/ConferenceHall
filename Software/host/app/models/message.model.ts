b0b86026d85c6c4ac60d7ec0de1ea00c21596bae
romanovrv <romanovrv@aquilon-st.ru>
2017-06-23 09:10:30 +0300

    message.model

---
+++ Software/host/app/models/message.model.ts
@@ -0,0 +1,20 @@
+﻿import { KeyModel } from './index';
+import { JsonProperty } from '../common/map-utils';
+
+/**
+ * Модель. Сообщения конференции
+ */
+export class MessageModel extends KeyModel {
+
+    active: boolean;
+    content: string;
+
+    constructor() {
+
+        super();
+
+        this.active = false;
+        this.content = undefined;
+        
+    }
+}
