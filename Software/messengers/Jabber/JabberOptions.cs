80e83b5291dd8ea4be58bf50a39baa0aa7bbf216
Stanislav Fiseyskiy <fiseyskiysv@aquilon-st.ru>
2017-06-27 11:23:50 +0300

    добавлены комментарии к полям

--- Software/messengers/Jabber/JabberOptions.cs
+++ Software/messengers/Jabber/JabberOptions.cs
@@ -5,10 +5,21 @@ namespace messengers.Jabber
     public class JabberOptions
     {
         // тип для отправки в jabbber клиент
+
+        /// <summary>
+        /// Учетная запись (jid) jabber с которой отправляется сообщение
+        /// </summary>
         public string JabberLogin { get; set; }
 
+        /// <summary>
+        /// Пароль к учетной запиcи jabber с которой отправляется сообщение
+        /// </summary>
         public string JabberPassword { get; set; }
 
+        /// <summary>
+        /// Задержка в милисекундах между процессом авторизации и отправки сообщения
+        /// протоколом xmpp
+        /// </summary>
         public Int32 JabberDelay { get; set; }
 
     }
