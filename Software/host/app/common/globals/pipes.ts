0e8edb26cabb68500129296cf24e0df4d1d5e528
romanovrv <romanovrv@aquilon-st.ru>
2017-07-14 10:53:12 +0300

    datexPipe

--- Software/host/app/common/globals/pipes.ts
+++ Software/host/app/common/globals/pipes.ts
@@ -1,5 +1,22 @@
 ﻿import { Pipe, PipeTransform } from '@angular/core';
-
+import * as moment from "moment";
+/**
+ * Форматирование dateTime pipe (реализация через moment.js)
+ */
+@Pipe({
+    name: "datex"
+})
+export class DatexPipe implements PipeTransform {
+    transform(value: any, format: string = ""): string {
+        // Try and parse the passed value.
+        let momentDate = moment(value);
+        // If moment didn't understand the value, return it unformatted.
+        if (!momentDate.isValid())
+            return value;
+        // Otherwise, return the date formatted as requested.
+        return momentDate.format(format);
+    }
+}
 
 /**
  * Переводит dateTime в utc
