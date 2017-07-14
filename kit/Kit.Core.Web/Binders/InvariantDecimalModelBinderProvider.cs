71ad7fc0ca4050fc11ac98cdf9e9f2fe22ec6866
romanovrv <romanovrv@aquilon-st.ru>
2017-07-14 14:38:09 +0300

    git subrepo clone --branch=netcore1.1 http://tfs:8080/tfs/ntcGroup/ntcProjects/_git/kit kit
    subrepo:
      subdir:   "kit"
      merged:   "1d5f5cc"
    upstream:
      origin:   "http://tfs:8080/tfs/ntcGroup/ntcProjects/_git/kit"
      branch:   "netcore1.1"
      commit:   "1d5f5cc"
    git-subrepo:
      version:  "0.3.1"
      origin:   "???"
      commit:   "???"

---
+++ kit/Kit.Core.Web/Binders/InvariantDecimalModelBinderProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Kit.Core.Web.Binders
+{
+    /// <summary>
+    /// Провайдер InvariantDecimalModelBinder'a
+    /// </summary>
+    public class InvariantDecimalModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context == null)
+                throw new ArgumentNullException(nameof(context));
+            
+            if (!context.Metadata.IsComplexType && 
+                    (context.Metadata.ModelType == typeof(decimal) || 
+                    context.Metadata.ModelType == typeof(decimal?) || 
+                    context.Metadata.ModelType == typeof(double) || 
+                    context.Metadata.ModelType == typeof(double?) ||
+                    context.Metadata.ModelType == typeof(float) || 
+                    context.Metadata.ModelType == typeof(float?))
+                )
+
+                return new InvariantDecimalModelBinder(context.Metadata.ModelType);
+
+            return null;
+        }
+    }
+}
