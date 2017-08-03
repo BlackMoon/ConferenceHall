8f174e510f16dac3af8547aa4f7670c13830bf6e
Роман Романов <romanovrv@aquilon-st.ru>
2017-08-03 07:54:14 +0000

    Updated subrepo_pull.sh

--- subrepo_pull.sh
+++ subrepo_pull.sh
@@ -4,5 +4,4 @@ export GIT_SUBREPO_ROOT="/c/program files/git/git-subrepo"
 export PATH=$GIT_SUBREPO_ROOT/lib:$PATH"
 export MANPATH=$GIT_SUBREPO_ROO/man:$MANPATH"
 
-#git subrepo clone http://portal:8080/tfs/ntcGroup/ntcProjects/_git/kit kit -b netcore1.1
 git subrepo pull --all
