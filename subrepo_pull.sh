9843e6166ea3cc201a36bedceb5c9dda48720b70
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 11:36:15 +0300

    if [ -n "$1" ]     then git checkout -B $1 fi

--- subrepo_pull.sh
+++ subrepo_pull.sh
@@ -4,4 +4,8 @@ export GIT_SUBREPO_ROOT="/c/program files/git/git-subrepo"
 export PATH=$GIT_SUBREPO_ROOT/lib:$PATH"
 export MANPATH=$GIT_SUBREPO_ROO/man:$MANPATH"
 
+if [ -n "$1" ]
+    then git checkout -B $1
+fi
+
 git subrepo pull --all
