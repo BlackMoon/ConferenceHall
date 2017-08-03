44c9d389852a496928d01e13b7796adb7d40d24b
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 15:25:05 +0300

    subrepo_pull.sh

--- subrepo_pull.sh
+++ subrepo_pull.sh
@@ -6,5 +6,9 @@ export GIT_SUBREPO_ROOT=$GIT_ROOT/git-subrepo
 export PATH=$GIT_ROOT/bin:$GIT_SUBREPO_ROOT/lib:$PATH"
 export MANPATH=$GIT_SUBREPO_ROO/man:$MANPATH"
 
+repo=$1
+
+cd $repo
+ls
 git subrepo pull --all
 
