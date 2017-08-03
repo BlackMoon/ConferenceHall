a12f0b5198ab0e60e4c856180f68ccacbc2f49a3
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 13:01:40 +0300

    # [2>&1] special flag for tfs build

--- subrepo_pull.sh
+++ subrepo_pull.sh
@@ -6,9 +6,10 @@ export GIT_SUBREPO_ROOT=$GIT_ROOT/git-subrepo
 export PATH=$GIT_ROOT/bin:$GIT_SUBREPO_ROOT/lib:$PATH"
 export MANPATH=$GIT_SUBREPO_ROO/man:$MANPATH"
 
+# [2>&1] special flag for tfs build
 if [ -n "$1" ]
 echo "ok"	
-    then git checkout --progress -B $1 
+    then git checkout --progress -B $1 2>&1 | Write-Host
 fi
 
-#git subrepo pull --all
+git subrepo pull --all
