be39ff4c3af90d0843d544b92b5f45330bde1d9a
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 16:38:04 +0300

    subrepo_push.sh

---
+++ subrepo_push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export GIT_SUBREPO_ROOT="/c/program files/git/git-subrepo"
+export PATH=$GIT_SUBREPO_ROOT/lib:$PATH
+export MANPATH=$GIT_SUBREPO_ROOT/man:$MANPATH
+
+git subrepo push --all
+
