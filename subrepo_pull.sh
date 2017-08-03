69bb917918b08e8dad38506c25f51443ea7aae3e
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 11:57:52 +0300

    --progress

--- subrepo_pull.sh
+++ subrepo_pull.sh
@@ -7,7 +7,7 @@ export PATH=$GIT_ROOT/bin:$GIT_SUBREPO_ROOT/lib:$PATH"
 export MANPATH=$GIT_SUBREPO_ROO/man:$MANPATH"
 
 if [ -n "$1" ]
-    then git checkout -B $1 --progress
+    then git checkout --progress -B $1 
 fi
 
 git subrepo pull --all
