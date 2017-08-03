2b7e5091ae9a31e8a26607145a98e1dfe0819bd1
romanovrv <romanovrv@aquilon-st.ru>
2017-08-03 16:42:32 +0300

    git subrepo commit

--- subrepo_push.sh
+++ subrepo_push.sh
@@ -4,5 +4,6 @@ export GIT_SUBREPO_ROOT="/c/program files/git/git-subrepo"
 export PATH=$GIT_SUBREPO_ROOT/lib:$PATH
 export MANPATH=$GIT_SUBREPO_ROOT/man:$MANPATH
 
+git subrepo commit kit
 git subrepo push --all
 
