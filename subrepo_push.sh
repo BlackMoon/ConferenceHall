d000a8e4d1b77bfb6209611f8772650948f8d942
romanovrv <romanovrv@aquilon-st.ru>
2017-08-04 11:59:37 +0300

    remove exports

--- subrepo_push.sh
+++ subrepo_push.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-export GIT_SUBREPO_ROOT="/c/program files/git/git-subrepo"
-export PATH=$GIT_SUBREPO_ROOT/lib:$PATH
-export MANPATH=$GIT_SUBREPO_ROOT/man:$MANPATH
-
 git subrepo push kit
 
